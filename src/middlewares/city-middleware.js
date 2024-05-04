314e3fec7b867784a0856329c8b1bf537a2be69c
Himanshu <himanshurahul1224@gmail.com>
2024-05-04 22:12:40 +0530

    Added City Middleware

--- src/middlewares/city-middleware.js
+++ src/middlewares/city-middleware.js
@@ -3,12 +3,10 @@ const { ErrorResponse } = require("../utils/common");
 const AppError = require("../utils/errors/app-error");
 
 function validateCreateRequest(req, res, next) {
-  if (!req.body.modelNumber) {
-    ErrorResponse.message = "Something went wrong while creating an airplane";
+  if (!req.body.name) {
+    ErrorResponse.message = "Something went wrong while creating a city";
     ErrorResponse.error = new AppError(
-      [
-        "Model Number was not found in the incoming request in the correct form",
-      ],
+      ["City name was not found in the incoming request in the correct form"],
       StatusCodes.BAD_REQUEST
     );
     return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
