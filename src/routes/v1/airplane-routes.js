9452053932e45ee843213165dd73b1f14f1f2ae6
Himanshu <himanshurahul1224@gmail.com>
2024-05-04 18:40:36 +0530

    Added get airplane API

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -3,13 +3,17 @@ const { AirplaneController } = require("../../controllers");
 const { AirplaneMiddlewares } = require("../../middlewares");
 
 const router = express.Router();
-
+// /api/v1/airplanes
 router.post(
   "/",
   AirplaneMiddlewares.validateCreateRequest,
   AirplaneController.createAirplane
 );
 
+// /api/v1/airplanes
 router.get("/", AirplaneController.getAirplanes);
+
+// /api/v1/airplanes/:id
+router.get("/:id", AirplaneController.getAirplane);
 
 module.exports = router;
--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -35,7 +35,26 @@ async function getAirplanes() {
   }
 }
 
+async function getAirplane(id) {
+  try {
+    const airplane = await airplaneRepository.get(id);
+    return airplane;
+  } catch (error) {
+    if (error.statusCode === StatusCodes.NOT_FOUND) {
+      throw new AppError(
+        "The requested airplane is not found",
+        error.statusCode
+      );
+    }
+    throw new AppError(
+      "Cannot fetch data of all the airplanes",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
 module.exports = {
   createAirplane,
   getAirplanes,
+  getAirplane,
 };
