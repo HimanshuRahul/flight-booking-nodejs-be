13672f892c9fc9a87f2b52a1039318de4f8aeb4e
Himanshu <himanshurahul1224@gmail.com>
2024-05-09 21:22:55 +0530

    FEAT: Added create flights API

---
+++ src/middlewares/flight-middlewares.js
@@ -0,0 +1,90 @@
+const { StatusCodes } = require("http-status-codes");
+const { ErrorResponse } = require("../utils/common");
+const AppError = require("../utils/errors/app-error");
+
+function validateCreateRequest(req, res, next) {
+  if (!req.body.flightNumber) {
+    ErrorResponse.message = "Something went wrong while creating an flight";
+    ErrorResponse.error = new AppError(
+      [
+        "Flight Number was not found in the incoming request in the correct form",
+      ],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  if (!req.body.airplaneId) {
+    ErrorResponse.message = "Something went wrong while creating an flight";
+    ErrorResponse.error = new AppError(
+      ["Airplane Id was not found in the incoming request in the correct form"],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  if (!req.body.departureAirportId) {
+    ErrorResponse.message = "Something went wrong while creating an flight";
+    ErrorResponse.error = new AppError(
+      [
+        "Departure Airport Id was not found in the incoming request in the correct form",
+      ],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+
+  if (!req.body.arrivalAirportId) {
+    ErrorResponse.message = "Something went wrong while creating an flight";
+    ErrorResponse.error = new AppError(
+      [
+        "Arrival Airport Id was not found in the incoming request in the correct form",
+      ],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+
+  if (!req.body.arrivalTime) {
+    ErrorResponse.message = "Something went wrong while creating an flight";
+    ErrorResponse.error = new AppError(
+      [
+        "Arrival Time was not found in the incoming request in the correct form",
+      ],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+
+  if (!req.body.departureTime) {
+    ErrorResponse.message = "Something went wrong while creating an flight";
+    ErrorResponse.error = new AppError(
+      [
+        "Departure Time was not found in the incoming request in the correct form",
+      ],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+
+  if (!req.body.price) {
+    ErrorResponse.message = "Something went wrong while creating an flight";
+    ErrorResponse.error = new AppError(
+      ["price was not found in the incoming request in the correct form"],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+
+  if (!req.body.totalSeats) {
+    ErrorResponse.message = "Something went wrong while creating an flight";
+    ErrorResponse.error = new AppError(
+      ["Total Seats was not found in the incoming request in the correct form"],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  next();
+}
+
+module.exports = {
+  validateCreateRequest,
+};
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   AirplaneMiddlewares: require("./airplane-middlewares"),
-  CityMiddlewares: require("./city-middleware"),
-  AirportMiddlewares: require("./airport-middleware"),
+  CityMiddlewares: require("./city-middlewares"),
+  AirportMiddlewares: require("./airport-middlewares"),
+  FlightMiddlewares: require("./flight-middlewares"),
 };
