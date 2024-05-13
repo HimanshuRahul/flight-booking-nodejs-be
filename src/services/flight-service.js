b41992039b973430e0578d7e02c5a89923070d66
Himanshu <himanshurahul1224@gmail.com>
2024-05-13 18:59:59 +0530

    FEAT: Added updateFlight API

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -87,7 +87,23 @@ async function getFlight(id) {
       throw new AppError("The requested Flight is not found", error.statusCode);
     }
     throw new AppError(
-      "Cannot fetch data of all the Flights",
+      "Cannot fetch data of the Flight",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
+async function updateSeats(data) {
+  try {
+    const response = await flightRepository.updateRemainingSeats(
+      data.flightId,
+      data.seats,
+      data.dec
+    );
+    return response;
+  } catch (error) {
+    throw new AppError(
+      "Cannot update data of the Flight",
       StatusCodes.INTERNAL_SERVER_ERROR
     );
   }
@@ -97,4 +113,5 @@ module.exports = {
   createFlight,
   getAllFlights,
   getFlight,
+  updateSeats,
 };
